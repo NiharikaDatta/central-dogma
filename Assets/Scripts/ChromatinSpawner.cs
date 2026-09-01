9ade3b85cbe8e6ba3d177e440b380e3299b25e2c
NiharikaDatta <college.niharika@gmail.com>
2026-09-01 13:47:08 +0530

    setting with new laptop

--- Assets/Scripts/ChromatinSpawner.cs
+++ Assets/Scripts/ChromatinSpawner.cs
@@ -56,7 +56,7 @@ public class ChromatinSpawner : MonoBehaviour
             {
                 return strand;
             }
-        }            
+        }           
         return dataList[0];   
     }
 }
